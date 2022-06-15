2279cca98b0f3d807705ee9d3aa6b6b5c25fc925
qinyingkai <q1y2k3@gmail.com>
2022-06-15 11:16:13 +0800

    change banner to nvim.bloody

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -25,7 +25,7 @@ function config.dashboard()
   local home = os.getenv('HOME')
   local db = require('dashboard')
   db.preview_command = 'cat | lolcat -F 0.3'
-  db.preview_file_path = home .. '/.config/nvim/static/neovim.cat'
+  db.preview_file_path = home .. '/.config/nvim/static/nvim.bloody'
   db.preview_file_height = 12
   db.preview_file_width = 80
   db.custom_center = {
